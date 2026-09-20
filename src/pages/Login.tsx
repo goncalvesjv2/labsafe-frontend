14b1bba4854dcf3c4d5e87a27d431050b9b8c04b
goncalvesjv2 <joaogoncalves3m@gmail.com>
2026-09-20 15:52:11 -0300

    criando uma tela de login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,19 @@
 function Login() {
     return (
-        <>
-            <h1>Página de Login</h1>
-        </>
+        <div className="h-screen bg-primary flex flex-col justify-center items-center">
+            <form className="bg-white w-md rounded-lg h-min p-4 flex flex-col gap-4">
+                <div className="flex flex-col gap-2">
+                    <label className="text-black text-small">E-mail:</label>
+                    <input className="border border-border rounded-lg p-2" type="text" />
+                </div>
+                <div className="flex flex-col gap-2">
+                    <label className="text-black text-small">Senha:</label>
+                    <input className="border border-border rounded-lg p-2" type="password" />
+                </div>
+                <button className="bg-primary p-3 rounded-lg text-white text-small font-bold">Entrar</button>
+                <p className="text-border text-small">Esqueci minha senha. Entre em contato com administrador.</p>
+            </form>
+        </div>
     )
 }
 

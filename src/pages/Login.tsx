9e5dc7e28bd158cf54f469fa9d8b02a12f0080b9
goncalvesjv2 <joaogoncalves3m@gmail.com>
2026-09-24 15:53:04 -0300

    feat: implementado a proteção de rota na página home

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -41,6 +41,7 @@ function Login() {
 
     async function handleSubmit(e: FormEvent) {
         e.preventDefault();
+        
         const token = await handleLogin();
 
         if (token) {
@@ -54,11 +55,11 @@ function Login() {
             <form onSubmit={handleSubmit} className="bg-white w-md rounded-lg h-min p-4 flex flex-col gap-4">
                 <div className="flex flex-col gap-2">
                     <label className="text-black text-small">E-mail:</label>
-                    <input className="border border-border rounded-lg p-2" type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <input className="border border-border rounded-lg p-2" type="text" value={email} onChange={(e) => { setEmail(e.target.value); setError(null);}} />
                 </div>
                 <div className="flex flex-col gap-2">
                     <label className="text-black text-small">Senha:</label>
-                    <input className="border border-border rounded-lg p-2" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <input className="border border-border rounded-lg p-2" type="password" value={password} onChange={(e) => { setPassword(e.target.value); setError(null); }} />
                 </div>
                 {error && <p className="text-red-500 text-small">{error}</p>}
                 <button type="submit" className="bg-primary p-3 rounded-lg text-white text-small font-bold">Entrar</button>
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
 import Home from "../pages/Home";
+import ProtectedRoutes from "./ProtectedRoutes";
 
 function AppRoutes() {
     return (
         <Routes>
             <Route path="/login" element={<Login />}/>
-            <Route path="/home" element={<Home/>} />
+            <Route element={<ProtectedRoutes />}>
+                <Route path="/home" element={<Home/>} />
+            </Route>
         </Routes>
     )
 }

19fba3b3bcf025c30f7f484902803eb7d1ead11e
goncalvesjv2 <joaogoncalves3m@gmail.com>
2026-09-25 11:46:07 -0300

    adicionando o .env no projeto

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -20,7 +20,7 @@ function Login() {
 
     async function handleLogin() {
         try {
-            const response = await fetch("http://localhost:3000/auth/login", {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/auth/login`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'

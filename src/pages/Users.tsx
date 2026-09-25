f347ba5da0acf21e952add604dd67397867f3efe
goncalvesjv2 <joaogoncalves3m@gmail.com>
2026-09-25 18:42:10 -0300

    feat: consumindo a API de usuários e exibindo em tela

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,6 +1,41 @@
 import { User } from "lucide-react";
+import { useEffect, useState } from "react";
+import type { IUsers } from "../interfaces/IUsers";
 
 function Users() {
+    const [users, setUsers] = useState<IUsers[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+
+    async function fetchUsers() {
+        try {
+            setLoading(true);
+            setError("");
+            const token = localStorage.getItem("token");
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/api/users`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+
+            if (!response.ok) {
+                throw new Error("Não foi possível carregar os usuários");
+            }
+
+            const data: IUsers[] = await response.json();
+            setUsers(data);
+        } catch (error) {
+            console.error(error);
+            setError("Não foi possível carregar os usuários");
+        } finally {
+            setLoading(false)
+        }
+    }
+    
+    useEffect(() => {
+        fetchUsers();
+    }, [])
+
     return (
         <>
             <h1 className="text-title font-bold">Usuários</h1>
@@ -12,17 +47,33 @@ function Users() {
                     Novo usuário
                 </button>
             </div>
-            <table className="w-full h-screen bg-white rounded-lg mt-4">
+            {loading && <p className="mt-4 text-small">Carregando usuários...</p>}
+
+            {error && <p className="mt-4 text-red-500 text-small">{error}</p>}
+            <table className="w-full bg-white rounded-lg mt-4">
                 <thead className="text-center">
                     <tr>
-                        <th className="text-normal">Nome</th>
-                        <th className="text-normal">E-mail</th>
-                        <th className="text-normal">Cargo</th>
-                        <th className="text-normal">Status</th>
-                        <th className="text-normal">Ações</th>
+                        <th className="text-normal p-2">Nome</th>
+                        <th className="text-normal p-2">E-mail</th>
+                        <th className="text-normal p-2">Cargo</th>
+                        <th className="text-normal p-2">Status</th>
+                        <th className="text-normal p-2">Ações</th>
                     </tr>
                 </thead>
-                <tbody></tbody>
+                <tbody className="text-center">
+                    {users.map((user) => (
+                        <tr key={user.id}>
+                            <td className="text-small p-2">{user.name}</td>
+                            <td className="text-small p-2">{user.email}</td>
+                            <td className="text-small p-2">{user.role}</td>
+                            <td className="text-small p-2">{user.ativo ? "Ativo" : "Inativo"}</td>
+                            <td className="flex gap-2 p-2 text-center justify-center">
+                                <button className="text-small p-2 border rounded-md text-border">Editar</button>
+                                <button className="text-small p-2 border rounded-md text-border">Desativar</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
             </table>
         </>
     )
